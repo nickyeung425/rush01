01a8dac38f7a5151627a025a5fd7854e53a5ccab
Nick Yeung <nickyeung425@hotmail.com>
2021-08-08 19:40:00 +0800

    test

--- rush01/utils.c
+++ rush01/utils.c
@@ -20,37 +20,6 @@ void ft_print_number(int n)
 	ft_print_char(n + '0');
 }
 
-void ft_print_int_array(int ***board, int **board_solution)
-{
-	int i;
-	int j;
-	int k;
-
-	i = 0;
-	j = 0;
-	k = 0;
-	while (i < G_SIZE)
-	{
-		while (j < G_SIZE)
-		{
-			while (k < board_solution[i][j])
-			{
-				while (k < board_solution[i][j])
-				{
-					ft_print_number(board[i][j][k]);
-					k++;
-				}
-				ft_print_char(' ');
-			}
-			k = 0;
-			j++;
-		}
-		ft_print_char('\n');
-		j = 0;
-		i++;
-	}
-}
-
 int convert_char_to_int(char c)
 {
 	{
