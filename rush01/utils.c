4f8f264a60afcd3bfa7301c4dfac4e430dbdd322
Beulah Lee <belee@u80z04s02.42kl.edu.my>
2021-08-08 19:00:43 +0800

    Beulah adds utils.c

--- rush01/utils.c
+++ rush01/utils.c
@@ -1,109 +1,102 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void ft_print_char(char c)
+void	ft_print_char(char c)
 {
-  write(1, &c, 1);
+	write(1, &c, 1);
 }
 
-void ft_print_str(char *str)
+void	ft_print_str(char *str)
 {
-  while(*str)
-  {
-    ft_print_char(*str);
-    str++;
-  }
+	while (*str)
+	{
+		ft_print_char(*str);
+		str++;
+	}
 }
 
-void ft_print_number(int n)
+void	ft_print_number(int n)
 {
-  ft_print_char(n + '0');
-}
-void ft_print_int_array(int ***board, int **board_solution)
-{
-  int i;
-  int j;
-  int k;
-  i = 0;
-  j = 0;
-  k = 0;
-  
-  while(i < g_size)
-  {
-    while(j < g_size)
-    {
-      while(k < board_solution[i][j])
-      {
-        while(k < board_solution[i][j])
-        {
-          ft_print_number(board[i][j][k]);
-          k++;
-        }
-        ft_print_char(' ');
-      }
-      k = 0;
-      j++;
-    }
-    ft_print_char('\n');
-    j = 0;
-    i++;
-  }
+	ft_print_char(n + '0');
 }
 
-int convert_char_to_int(char c)
+void	ft_print_int_array(int ***board, int **board_solution)
 {
-  if (c == '1')
-  {
-    return 1;
-  } else if (c == '2')
-  {
-    return 2;
-  } else if (c == '3')
-  {
-    return 3;
-  } else if (c == '4')
-  {
-    return 4;
-  } else if (c == '5')
-  {
-    return 5;
-  } else if (c == '6')
-  {
-    return 6;
-  } else if (c == '7')
-  {
-    return 7;
-  } else if (c == '8')
-  {
-    return 8;
-  } else if (c == '9')
-  {
-    return 9;
-  }
-  return 0;
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while (i < g_size)
+	{
+		while (j < g_size)
+		{
+			while (k < board_solution[i][j])
+			{
+				while (k < board_solution[i][j])
+				{
+					ft_print_number(board[i][j][k]);
+					k++;
+				}
+				ft_print_char(' ');
+			}
+			k = 0;
+			j++;
+		}
+		ft_print_char('\n');
+		j = 0;
+		i++;
+	}
 }
 
-
-void ft_print_int_array_result(int ***board)
+int	convert_char_to_int(char c)
 {
-  int i;
-  int j;
-  i = 0;
-  j = 0;
-  while(i < g_size)
-  {
-    while(j < g_size)
-    {
-      ft_print_number(board[i][j][0]);
-      if (j != g_size - 1)
-      {
-        ft_print_char(' ');
-      }
-      j++;
-    }
-    ft_print_char('\n');
-    j = 0;
-    i++;
-  }
-  ft_print_char('\n');
+	{
+		if (c == '1')
+			return (1);
+		else if (c == '2')
+			return (2);
+		else if (c == '3')
+			return (3);
+		else if (c == '4')
+			return (4);
+		else if (c == '5')
+			return (5);
+		else if (c == '6')
+			return (6);
+		else if (c == '7')
+			return (7);
+		else if (c == '8')
+			return (8);
+		else if (c == '9')
+			return (9);
+	}
+	return (0);
+}
+
+void	ft_print_int_array_result(int ***board)
+{		
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (i < g_size)
+	{
+		while (j < g_size)
+		{
+			ft_print_number(board[i][j][0]);
+			if (j != g_size - 1)
+		}
+		ft_print_char(' ');
+		}
+	j++;
+	}	
+	ft_print_char('\n');
+	j = 0;
+	i++;
+}
+ft_print_char('\n');
 }
