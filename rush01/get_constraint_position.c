952cadf609b77ed7489587a5584b00530abd277e
Wing chee Yeung <wyeung@u90z08s03.42kl.edu.my>
2021-08-08 19:15:22 +0800

    1

--- rush01/get_constraint_position.c
+++ rush01/get_constraint_position.c
@@ -1,23 +1,25 @@
-int get_i(int index)
+int	get_i(int index)
 {
-  int i;
-  if (index < g_size)
-    i = 0;
-  else if (g_size <= index && index < (g_size * 2))
-    i = 3;
-  else
-    i = index % g_size;
-  return (i);
+	int	i;
+
+	if (index < g_size)
+		i = 0;
+	else if (g_size <= index && index < (g_size * 2))
+		i = 3;
+	else
+		i = index % g_size;
+	return (i);
 }
 
-int get_j(int index)
+int	get_j(int index)
 {
-  int j;
-  if (index < g_size * 2)
-    j = index % g_size;
-  else if (g_size * 2 <= index && index < g_size * 3)
-    j = 0;
-  else
-    j = 3;
-  return (j);
+	int	j;
+
+	if (index < g_size * 2)
+		j = index % g_size;
+	else if (g_size * 2 <= index && index < g_size * 3)
+		j = 0;
+	else
+		j = 3;
+	return (j);
 }
