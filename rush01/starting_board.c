844d62c6f47e6c41daa4ee6ba815326f37d1480d
Tan Jun ren <tjun-ren@u90z03s03.42kl.edu.my>
2021-08-08 17:23:51 +0800

    norm

--- rush01/starting_board.c
+++ rush01/starting_board.c
@@ -1,58 +1,55 @@
 #include <stdlib.h>
 
-int ***ft_starting_board()
+int	***ft_starting_board(void)
 {
-  int ***board = NULL;
-  board = (int ***)malloc(sizeof (int **) * g_size);
+	int	i;
+	int	j;
+	int	k;
+	int	***board;
 
-  int i;
-  int j;
-  int k;
-  i = 0;
-  j = 0;
-  k = 0;
-
-  while (i < g_size)
-  {
-    board[i] = (int **)malloc(sizeof (int *) * g_size);
-    while(j < g_size)
-    {
-      board[i][j] = (int *)malloc(sizeof (int) * g_size);
-      while(k < g_size)
-      {
-        board[i][j][k] = k + 1;
-        k++;
-      }
-      k = 0;
-      j++;
-    }
-    j = 0;
-    i++;
-  }
-  return board;
+	i = 0;
+	j = 0;
+	k = 0;
+	board = (int ***)malloc(sizeof (int **) * g_size);
+	while (i < g_size)
+	{
+		board[i] = (int **)malloc(sizeof (int *) * g_size);
+		while (j < g_size)
+		{
+			board[i][j] = (int *)malloc(sizeof (int) * g_size);
+			while (k < g_size)
+			{
+				board[i][j][k] = k + 1;
+				k++;
+			}
+			k = 0;
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (board);
 }
 
-
-int **ft_starting_board_solution()
+int	**ft_starting_board_solution(void)
 {
-  int **board = NULL;
-  board = (int **)malloc(sizeof (int *) * g_size);
+	int	i;
+	int	j;
+	int	**board;
 
-  int i;
-  int j;
-  i = 0;
-  j = 0;
-
-  while (i < g_size)
-  {
-    board[i] = (int *)malloc(sizeof (int) * g_size);
-    while(j < g_size)
-    {
-      board[i][j] = g_size;
-      j++;
-    }
-    j = 0;
-    i++;
-  }
-  return board;
+	i = 0;
+	j = 0;
+	board = (int **)malloc(sizeof (int *) * g_size);
+	while (i < g_size)
+	{
+		board[i] = (int *)malloc(sizeof (int) * g_size);
+		while (j < g_size)
+		{
+			board[i][j] = g_size;
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (board);
 }
